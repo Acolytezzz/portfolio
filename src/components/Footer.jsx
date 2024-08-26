5f15d76218ce07d0e37e5d1943e76774d67c0c20
Aman Shrestha <stormaker777@gmail.com>
2024-08-27 08:21:19 +1000

    Test for quick change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="flex flex-col items-center">
         <hr className="h-1 bg-black dark:bg-white w-screen mb-3"/>
-        <p className="text-2xl font-Cantora mb-5 dark:text-white">Copyright © 2050 - All Rights Reserved</p>
+        <p className="text-2xl font-Cantora mb-5 dark:text-white">Copyright © 2024 - All Rights Reserved</p>
     </div>
   )
 }
